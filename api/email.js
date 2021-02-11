3cdac4097eb702a207b2dd1b0dc357d9659b5bd6
David Mesman <david@uptimeheroes.com>
2021-02-11 10:32:33 +0100

    fix( email.js ): Used an env variable to control the recipient of the contact email

--- api/email.js
+++ api/email.js
@@ -3,13 +3,18 @@ const sgMail = require('@sendgrid/mail');
 const sendMail = function(from) {
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
   const msg = {
-    to: 'launch@thehaguetech.com',
+    to: process.env.CONTACT_MAIL,
     from: 'website@thehaguetech.com',
     subject: 'Email from thehaguetech.com',
     html: `${from.name} (${from.email}) sent the following message:<br /><hr /><br />` +
           from.message.replace("\n", '<br/>') + "<br /><br />"
   };
-  sgMail.send(msg);
+  sgMail.send(msg).then((result) => {
+    console.log('Mail sent');
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 }
 
 module.exports = { sendMail }
